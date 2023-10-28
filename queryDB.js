600a98a2bd35867753b08423be1d5c2bb44ed9c1
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-27 22:40:21 -0700

    added queryDB as function

--- queryDB.js
+++ queryDB.js
@@ -1,17 +1,22 @@
 import fs from 'fs';
 
-let info = [];
-if(fs.existsSync("db.json")){
-
-    fs.readFile("db.json", function(err, data){
-        if(err){
-            console.log(err);
+function queryDB(){
+    try{
+        let info = [];
+        if(fs.existsSync("db.json")){
+        
+            fs.readFile("db.json", function(err, data){
+                if(err){
+                    console.log(err);
+                }
+                info = JSON.parse(data.toString())
+                console.log(info);
+            })
+        }else {
+            console.log("No Data Available")
         }
-
-        info = JSON.parse(data.toString())
-
-        console.log();
-    })
-}else {
-    console.log("No Data Available")
+    }catch(error){
+        console.log("Error in reading file", error)
+    }
 }
+
