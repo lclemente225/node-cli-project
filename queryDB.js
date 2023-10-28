d441ca1da28b113c0e4bfb3ee53d30055e9dd90a
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-10-27 22:33:37 -0700

    added info array to queryDB

--- queryDB.js
+++ queryDB.js
@@ -1,9 +1,17 @@
 import fs from 'fs';
 
+let info = [];
+if(fs.existsSync("db.json")){
 
-fs.readFile("db.json", function(err, data){
-    if(err){
-        console.log(err);
-    }
-    console.log(data.toString());
-})
+    fs.readFile("db.json", function(err, data){
+        if(err){
+            console.log(err);
+        }
+
+        info = JSON.parse(data.toString())
+
+        console.log();
+    })
+}else {
+    console.log("No Data Available")
+}
